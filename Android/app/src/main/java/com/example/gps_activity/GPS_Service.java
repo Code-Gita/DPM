4eadb59694229857a235c71df66064e1710a03f8
Code-Gita <codegita@gmail.com>
2019-12-27 13:43:25 +0530

    Fix for imei +  Phone number + permission request

--- Android/app/src/main/java/com/example/gps_activity/GPS_Service.java
+++ Android/app/src/main/java/com/example/gps_activity/GPS_Service.java
@@ -1,9 +1,12 @@
 package com.example.gps_activity;
 
 import android.annotation.SuppressLint;
+import android.provider.Settings;
+import android.os.Build;
 import android.app.Service;
 import android.content.Intent;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.IBinder;
 import android.os.Looper;
 import android.util.Log;
@@ -13,24 +16,28 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationAvailability;
 
-import java.sql.Date;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
+import java.util.Arrays;
 import java.util.TimeZone;
+import android.text.TextUtils;
+import android.Manifest;
+import android.content.pm.PackageManager;
 
-//import java.util.Date;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 
 public class GPS_Service extends Service {
 
-    //private LocationListener locListner;
-    //private LocationManager locManager;
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
     LocationCallback locationCallback;
-    //private int runOnce=0;
+    LocationManager locationManager;
     private String TAG= "Debug";
-
+    private static final int REQUEST_CODE_LOCATION = 1000;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -50,22 +57,34 @@ public class GPS_Service extends Service {
         final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
         sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
 
+        if(isLocationServiceAvailbale()==false)
+        {
+            Intent i = new Intent("location_update");
+            String locationStopped ="LOCATION_STOPPED";
+            i.putExtra("coordinates",locationStopped);
+            sendBroadcast(i);
+        }
+
         locationCallback = new LocationCallback()
         {
+            Intent i = new Intent("location_update");
+            @Override
+            public void onLocationAvailability(LocationAvailability locationAvailability) {
+                if (locationAvailability.isLocationAvailable() == false) {
+                    String locationStopped ="LOCATION_STOPPED";
+                    i.putExtra("coordinates",locationStopped);
+                    sendBroadcast(i);
+                }
+            }
+
             @Override
             public void onLocationResult(LocationResult locationResult) {
-                //super.onLocationResult(locationResult);
                 for (Location location : locationResult.getLocations())
                 {
+                    Log.d(TAG, "onCreate: location service availalble 2: "+isLocationServiceAvailbale());
                     Log.d(TAG, "onLocationResult: Latitude : "+String.valueOf(location.getLatitude())+
                             " \t Longitude : "+String.valueOf(location.getLongitude()));
-                    Intent i = new Intent("location_update");
 
-                    /*i.putExtra("coordinates",location.getLongitude()+"#"+location.getLatitude()+
-                            "#"+new java.text.SimpleDateFormat("yyyy-MM-dd").format(location.getTime())+
-                            "T"+new java.text.SimpleDateFormat("HH:mm:ss.SSS").format(location.getTime())+
-                            "#"+ String.valueOf(location.getSpeed()));*/
-                    //String time = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS").format(location.getTime());
 
                     i.putExtra("coordinates",location.getLongitude()+"#"+location.getLatitude()+
                             "#"+sdf.format(location.getTime())+
@@ -78,10 +97,11 @@ public class GPS_Service extends Service {
             }
         };
 
-
         try
         {
+            //Log.d(TAG, "onCreate: location service availalble 9: "+isLocationServiceAvailbale());
             fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            //Log.d(TAG, "onCreate: location service availalble 10: "+isLocationServiceAvailbale());
             fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
         }
         catch (Exception e)
@@ -91,70 +111,35 @@ public class GPS_Service extends Service {
         }
 
     }
-/*@SuppressLint("MissingPermission")
-    @Override
-    public void onCreate() {
-        Log.d("Debug", "onCreate: Inside oncreate service ");
-        locListner = new LocationListener() {
-
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d("Debug", "onLocationChanged: calling location Changed ");
-                Intent i = new Intent("location_update");
-                i.putExtra("coordinates",location.getLongitude()+"#"+location.getLatitude()+
-                        "#"+new java.text.SimpleDateFormat("yyyy-MM-dd").format(location.getTime())+
-                        "T"+new java.text.SimpleDateFormat("HH:mm:ss.SSS").format(location.getTime())+
-                        "#"+ String.valueOf(location.getSpeed()));
-                //String time = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss.SSS").format(location.getTime());
-                sendBroadcast(i);
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                Log.d("Debug", "onStatusChanged: ");
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-                Log.d("Debug", "onProviderEnabled: ");
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Log.d("Debug", "onProviderDisabled: ");
-                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(i);
-            }
-        };
-
-        Log.d("Debug", "onCreate: here 1 ");
-        locManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-
-        Log.d("Debug", "onCreate: here 2 ");
-
-        locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10000,0,locListner);
-
-        //GpsStatus gps_stat;
-        //Log.d("Debug", "onCreate: "+locManager.getGpsStatus(GpsStatus gps_stat));
-        Log.d("Debug", "onCreate: "+locManager.getProviders(true));
-        Log.d("Debug", "onCreate: "+locManager.getProvider("gps"));
-        Log.d("Debug", "onCreate: "+locManager.getLastKnownLocation("network"));
-
-
-    }*/
 
     @Override
     public void onDestroy() {
         super.onDestroy();
 
-        /*if(locManager!=null)
-        {
-            locManager.removeUpdates(locListner);
-        }*/
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
+
+    public boolean isLocationServiceAvailbale()
+    {
+        String locationProviders;
+        boolean isAvailable = false;
+        int locationMode = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(getApplicationContext().getContentResolver(), Settings.Secure.LOCATION_MODE);
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            isAvailable = (locationMode != Settings.Secure.LOCATION_MODE_OFF);
+        } else {
+            locationProviders = Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            isAvailable = !TextUtils.isEmpty(locationProviders);
+        }
+        boolean coarsePermissionCheck = (getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED);
+        boolean finePermissionCheck = (getApplicationContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
+
+        return isAvailable && (coarsePermissionCheck || finePermissionCheck);
+    }
+
 }
--- Android/app/src/main/java/com/example/gps_activity/MainActivity.java
+++ Android/app/src/main/java/com/example/gps_activity/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -25,24 +26,37 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import android.telephony.TelephonyManager;
+
 import android.provider.Settings.Secure;
+import android.widget.Toast;
+import java.util.Arrays;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startActivity, stopActivity;
-    private TextView latitude,longitude,speed,gpsTime,imeiNumber,requestView,responseView;
+    private TextView latitude, longitude, speed, gpsTime, imeiNumber, phoneNumber, requestView, responseView;
     private BroadcastReceiver broadcastReciever;
     private RequestQueue reqQueue;
     private StringRequest strRequest;
+    private String ImeiNumber,mobNumber;
     private StringBuilder websiteURL = new StringBuilder(" ");
-    private static final int REQUEST_CODE=1000;
+    private static final int REQUEST_CODE_LOCATION = 1000;
+    private static final int REQUEST_CODE_DEVICE_INFO = 2000;
+    private static final int REQUEST_CODE_INTERNET = 3000;
+    private LocationManager locationManager;
+    private boolean isGPS=false;
+    private String TAG ="debug";
+    private Intent intent1;
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        if(broadcastReciever == null)
-        {
+        if (broadcastReciever == null) {
             broadcastReciever = new BroadcastReceiver() {
                 @Override
                 public void onReceive(Context context, Intent intent) {
@@ -56,43 +70,86 @@ public class MainActivity extends AppCompatActivity {
         registerReceiver(broadcastReciever, new IntentFilter("location_update"));
     }
 
+
+    @SuppressLint("MissingPermission")
     private void sendRequestGetResponse(Intent i) {
-        reqQueue = Volley.newRequestQueue(getApplicationContext());
-        String cordinates = (String) i.getExtras().get("coordinates");
-        String usrLat = cordinates.split("#")[0];
-        String usrLong = cordinates.split("#")[1];
-        String usrGpsDateTime = cordinates.split("#")[2];
-        String usrSpeed = cordinates.split("#")[3];
-        Log.d("Debug", "sendRequestGetResponse: "+usrLat+ " usrLong : "+usrLong );
+        if(((String) i.getExtras().get("coordinates")).equalsIgnoreCase( "LOCATION_STOPPED")==Boolean.TRUE)
+        {
+            Log.d("Debug", "sendRequestGetResponse: location stopped");
 
-        String tempUrl = "?aid="+Secure.getString(this.getContentResolver(), Secure.ANDROID_ID) +
-                "&lat="+usrLat+"&longitude="+usrLong+"&time="+usrGpsDateTime+"&s="+usrSpeed;
-        Log.d("Debug", "sendRequestGetResponse: tmpurl :"+tempUrl);
-        latitude.setText(usrLat);
-        longitude.setText(usrLong);
-        speed.setText(usrSpeed);
-        gpsTime.setText(usrGpsDateTime);
-
-        websiteURL.append("http://thetrackme.com/gps_logger.php");
-        websiteURL.append(tempUrl);
-        Log.d("Request", websiteURL.toString());
-
-        requestView.setText(websiteURL);
-        strRequest = new StringRequest(Request.Method.GET, websiteURL.toString(), new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.i("Response", "onResponse:  "+response.toString());
+            if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+            {
+                Log.d(TAG, "sendRequestGetResponse: GPS provider enabled");
             }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.i("ERROR", "onErrorResponse: "+error.toString());
-
+            else
+            {
+                Log.d(TAG, "sendRequestGetResponse: GPS provider disabled");
+                try
+                {
+                    intent1 = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent1);
+                }
+                catch( Exception e)
+                {
+                    Log.d(TAG, "sendRequestGetResponse: " + e.getMessage());
+                    e.printStackTrace();
+                }
             }
-        });
+        }
+        else {
+            Log.d("Debug", "sendRequestGetResponse: location else :  "+(String)i.getExtras().get("coordinates"));
 
-        reqQueue.add(strRequest);
-        websiteURL.setLength(0);
+            reqQueue = Volley.newRequestQueue(getApplicationContext());
+            String cordinates = (String) i.getExtras().get("coordinates");
+            String usrLat = cordinates.split("#")[0];
+            String usrLong = cordinates.split("#")[1];
+            String usrGpsDateTime = cordinates.split("#")[2];
+            String usrSpeed = cordinates.split("#")[3];
+            Log.d("Debug", "sendRequestGetResponse: " + ImeiNumber + "\n phone number : " + mobNumber);
+
+            Log.d("Debug", "sendRequestGetResponse: " + usrLat + " usrLong : " + usrLong);
+
+            String tempUrl = "?aid=" + Secure.getString(this.getContentResolver(), Secure.ANDROID_ID) +
+                    "&lat=" + usrLat + "&longitude=" + usrLong + "&time=" + usrGpsDateTime + "&s=" + usrSpeed + "&Imei=" + ImeiNumber + "&mob=" + mobNumber;
+
+            Log.d("Debug", "sendRequestGetResponse: tmpurl :" + tempUrl);
+            latitude.setText(usrLat);
+            longitude.setText(usrLong);
+            speed.setText(usrSpeed);
+            gpsTime.setText(usrGpsDateTime);
+
+            TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+
+            if (telephonyManager != null) {
+                ImeiNumber = telephonyManager.getImei();
+            }
+            mobNumber = telephonyManager.getLine1Number();
+
+            imeiNumber.setText(ImeiNumber);
+            phoneNumber.setText(mobNumber);
+
+            websiteURL.append("http://thetrackme.com/gps_logger.php");
+            websiteURL.append(tempUrl);
+            Log.d("Request", websiteURL.toString());
+
+            requestView.setText(websiteURL);
+            strRequest = new StringRequest(Request.Method.GET, websiteURL.toString(), new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("Response", "onResponse:  " + response.toString());
+                }
+            }, new Response.ErrorListener() {
+
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.i("ERROR", "onErrorResponse: " + error.toString());
+
+                }
+            });
+
+            reqQueue.add(strRequest);
+            websiteURL.setLength(0);
+        }
     }
 
     @Override
@@ -107,6 +164,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Debug", "onCreate: In onCreate ");
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
         startActivity = (Button) findViewById(R.id.startServiceBtn);
         stopActivity = (Button) findViewById(R.id.stopServiceBtn);
         latitude = (TextView) findViewById(R.id.t2);
@@ -115,10 +175,24 @@ public class MainActivity extends AppCompatActivity {
         gpsTime = (TextView) findViewById(R.id.t4);
         requestView = (TextView) findViewById(R.id.RequestView);
         responseView = (TextView) findViewById(R.id.ResponseView);
+        imeiNumber = (TextView) findViewById(R.id.Imei_txt5);
+        phoneNumber = (TextView) findViewById((R.id.txt6));
 
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION);
+        }
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.d("Debug", "onCreate:  granting internet permission");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, REQUEST_CODE_INTERNET);
+        }
+
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.d("Debug", "onCreate:  granting internet permission");
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_CODE_DEVICE_INFO);
         }
 
         enableButton();
@@ -146,7 +220,9 @@ public class MainActivity extends AppCompatActivity {
                 longitude.setText("");
                 speed.setText("");
                 gpsTime.setText("");
+                imeiNumber.setText("");
                 responseView.setText("");
+                phoneNumber.setText("");
                 startActivity.setEnabled(true);
                 stopActivity.setEnabled(false);
             }
@@ -159,20 +235,53 @@ public class MainActivity extends AppCompatActivity {
         stopActivity.setEnabled(false);
     }
 
+    private void disableButton() {
+        Log.d("Debug", "disableButton: Start ");
+        Intent i = new Intent(getApplicationContext(), GPS_Service.class);
+        stopService(i);
+        startActivity.setEnabled(true);
+        stopActivity.setEnabled(false);
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d("Debug", "onRequestPermissionsResult: requestCode : " +requestCode);
+        Log.d("Debug", "onRequestPermissionsResult: requestCode : " +requestCode + " \n grant results : "+ Arrays.toString(grantResults));
         switch(requestCode)
         {
-            case REQUEST_CODE:
+            case REQUEST_CODE_LOCATION:
             {
                 if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
                 {
-                    if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+                    if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
                     {
-                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+                        Log.d("Debug", "onRequestPermissionsResult: permission not granted");
+                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE_LOCATION);
+                    }
+                    else
+                    {
+                        /*Log.d("debug", "onRequestPermissionsResult: location permision denied 1");
+                        Toast.makeText(getApplicationContext(),"Please switch on the location servies to start the service again 1",Toast.LENGTH_LONG).show();
+                        disableButton();*/
+                    }
+                    enableButton();
+                }
+                else if(grantResults[0] == PackageManager.PERMISSION_DENIED)
+                {
+                    Log.d("debug", "onRequestPermissionsResult: location permision denied");
+                    Toast.makeText(getApplicationContext(),"Please switch on the location servies to start the service again",Toast.LENGTH_LONG).show();
+                    disableButton();
+
+                }
+                break;
+            }
+            case REQUEST_CODE_INTERNET:
+            {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.checkSelfPermission(this,Manifest.permission.INTERNET)!=PackageManager.PERMISSION_GRANTED )
+                    {
+                        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET},REQUEST_CODE_INTERNET);
                     }
                     enableButton();
                 }
@@ -180,7 +289,39 @@ public class MainActivity extends AppCompatActivity {
                 {
 
                 }
+                break;
             }
+            case REQUEST_CODE_DEVICE_INFO:
+            {
+                Log.d("Debug", "onRequestPermissionsResult: here XX");
+                {
+                    // If request is cancelled, the result arrays are empty.
+                    if (grantResults.length > 0
+                            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                        // permission was granted, yay! Do the
+                        // contacts-related task you need to do.
+
+                    } else {
+
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.READ_PHONE_STATE},
+                                REQUEST_CODE_DEVICE_INFO);
+
+                        intent1 = new Intent(Settings.ACTION_DEVICE_INFO_SETTINGS);
+                        startActivity(intent1);
+
+                        // permission denied, boo! Disable the
+                        // functionality that depends on this permission.
+                    }
+                    return;
+                }
+                //break;
+            }
+
+            default:
+                break;
+
         }
     }
 }
